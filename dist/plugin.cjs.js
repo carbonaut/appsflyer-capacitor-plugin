9991b6d969d4ab8dae534113734b6255255011f6
Arthur Floriani <artfloriani@gmail.com>
2024-07-29 12:21:43 -0300

    chore: adds dist folder to github

---
+++ dist/plugin.cjs.js
@@ -0,0 +1,40 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var core = require('@capacitor/core');
+
+exports.AFConstants = void 0;
+(function (AFConstants) {
+    AFConstants["onConversionDataSuccess"] = "onConversionDataSuccess";
+    AFConstants["onConversionDataFail"] = "onConversionDataFail";
+    AFConstants["onAppOpenAttribution"] = "onAppOpenAttribution";
+    AFConstants["onAttributionFailure"] = "onAttributionFailure";
+    AFConstants["CONVERSION_CALLBACK"] = "conversion_callback";
+    AFConstants["OAOA_CALLBACK"] = "oaoa_callback";
+    AFConstants["UDL_CALLBACK"] = "udl_callback";
+})(exports.AFConstants || (exports.AFConstants = {}));
+
+class AppsFlyerConsentClass {
+    constructor(isUserSubjectToGDPR, hasConsentForDataUsage, hasConsentForAdsPersonalization) {
+        this.isUserSubjectToGDPR = isUserSubjectToGDPR;
+        this.hasConsentForDataUsage = hasConsentForDataUsage;
+        this.hasConsentForAdsPersonalization = hasConsentForAdsPersonalization;
+    }
+    static forGDPRUser(hasConsentForDataUsage, hasConsentForAdsPersonalization) {
+        return new AppsFlyerConsentClass(true, hasConsentForDataUsage, hasConsentForAdsPersonalization);
+    }
+    static forNonGDPRUser() {
+        return new AppsFlyerConsentClass(false);
+    }
+}
+const AppsFlyerConsent = {
+    forGDPRUser: AppsFlyerConsentClass.forGDPRUser,
+    forNonGDPRUser: AppsFlyerConsentClass.forNonGDPRUser
+};
+
+const AppsFlyer = core.registerPlugin('AppsFlyerPlugin', {});
+
+exports.AppsFlyer = AppsFlyer;
+exports.AppsFlyerConsent = AppsFlyerConsent;
+//# sourceMappingURL=plugin.cjs.js.map
